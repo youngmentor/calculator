be583ba15f5b33adc3a9f46aebf08981ecba2807
youngmentor <olaleresuliton43@gmail.com>
2023-10-04 23:15:55 +0100

    make changes to the code base

--- calculate.js
+++ calculate.js
@@ -1,7 +1,14 @@
-let result=document.getElementById("inputtext")
-let calculate=(numbers)=>{result.value+=numbers}
-let simplify=()=>{try{result.value=eval(result.value)}
-                 catch(err){alert("syntax error" )}};
-function clr(){result.value =""};
-function del(){result.value=result.value.slice(0,-1)}
-function power(){result.value =" "}; 
+ let result = document.getElementById("inputtext");
+let calculate = (numbers) => {result.value += numbers;};
+let simplify = () => {
+  try { result.value = eval(result.value);
+    if (isNaN(result.value)) {
+      throw new Error("Invalid expression");
+    }
+  } catch (err) {
+    result.value = "Syntax Error: " + err.message;
+  }
+};
+function clr() {result.value = "";}
+function del() {result.value = result.value.slice(0, -1);}
+function power() {result.value = " ";}
