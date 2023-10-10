700cdae498b1f3e618f3f338b9baaa096040e157
youngmentor <olaleresuliton43@gmail.com>
2023-10-10 19:36:06 +0100

    make changes

--- calculate.js
+++ calculate.js
@@ -1,14 +1,24 @@
- let result = document.getElementById("inputtext");
-let calculate = (numbers) => {result.value += numbers;};
-let simplify = () => {
-  try { result.value = eval(result.value);
-    if (isNaN(result.value)) {
-      throw new Error("Invalid expression");
-    }
-  } catch (err) {
-    result.value = "Syntax Error: " + err.message;
-  }
+let inputText = document.getElementById("inputtext");
+let resultElement = document.getElementById("result");
+let calculate = (value) => {
+    // Append the value to the input screen
+    inputText.value += value;
 };
-function clr() {result.value = "";}
-function del() {result.value = result.value.slice(0, -1);}
-function power() {result.value = "  ";}
+let simplify = () => {
+    try {
+        let expression = inputText.value;
+        let result = eval(expression);
+        if (isNaN(result)) {
+            throw new Error("Invalid expression");
+        }
+        resultElement.textContent = result;
+        // Clear the input screen
+        inputText.value = "";
+    } catch (err) {
+        resultElement.textContent = "Syntax Error: " + err.message;
+    }
+};
+function clr() {inputText.value = ""; resultElement.textContent = "Result: ";}
+function del() {inputText.value = inputText.value.slice(0, -1);}
+
+function power() {inputText.value = ""; resultElement.textContent = "" ;}
